da36a40c9ba2cfd96d89f9e8036f7581febf5ca5
Daniel Duberg <danielduberg@gmail.com>
2024-01-15 14:55:20 +0100

    Changes for VT24

--- robp_boot_camp_motors/src/motors.cpp
+++ robp_boot_camp_motors/src/motors.cpp
@@ -1,116 +1,115 @@
 // ROBP
 #include <robp_boot_camp_motors/kobuki_motors.h>
-#include <robp_msgs/DutyCycles.h>
-#include <robp_msgs/Encoders.h>
+
+#include <robp_interfaces/msg/duty_cycles.hpp>
+#include <robp_interfaces/msg/encoders.hpp>
 
 // ROS
-#include <geometry_msgs/Twist.h>
-#include <ros/ros.h>
+#include <geometry_msgs/msg/twist.hpp>
+#include <rclcpp/rclcpp.hpp>
 
 // STL
 #include <memory>
-#include <random>
 #include <vector>
 
-class Motors {
+class Motors : public rclcpp::Node
+{
  public:
-  Motors(ros::NodeHandle &nh) {
-    kobuki_motors_ = std::make_unique<KobukiMotors>();
+	Motors() : Node("motors")
+	{
+		kobuki_motors_ = std::make_unique<KobukiMotors>();
 
-    encoders_pub_ = nh.advertise<robp_msgs::Encoders>("/motor/encoders", 1);
-    twist_pub_ =
-        nh.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 1);
+		encoders_pub_ =
+		    this->create_publisher<robp_interfaces::msg::Encoders>("/motor/encoders", 1);
+		twist_pub_ = this->create_publisher<geometry_msgs::msg::Twist>(
+		    "/mobile_base/commands/velocity", 1);
 
-    duty_cycles_sub_ = nh.subscribe("/motor/duty_cycles", 1,
-                                    &Motors::dutyCyclesCallback, this);
-  }
+		duty_cycles_sub_ = this->create_subscription<robp_interfaces::msg::DutyCycles>(
+		    "/motor/duty_cycles", 1,
+		    std::bind(&Motors::dutyCyclesCallback, this, std::placeholders::_1));
 
-  // [0] corresponds to left wheel, [1] corresponds to right wheel
-  void dutyCyclesCallback(robp_msgs::DutyCycles::ConstPtr const &msg) {
-    duty_cycles_[0] = msg->duty_cycle_left;
-    duty_cycles_[1] = msg->duty_cycle_right;
-    duty_cyles_time_ = ros::Time::now();
-  }
+		duty_cyles_time_ = this->now();
 
-  void updateMotors() {
-    // if more than 0.5 seconds have passed and no messages have been received,
-    // shutdown the motors
-    if ((ros::Time::now() - duty_cyles_time_).toSec() > 0.5) {
-      duty_cycles_[0] = 0.0;
-      duty_cycles_[1] = 0.0;
-    }
+		using namespace std::chrono_literals;
+		timer_ = this->create_wall_timer(1s / 10.0, std::bind(&Motors::updateMotors, this));
+	}
 
-    if (1.0 < std::abs(duty_cycles_[0]) || 1.0 < std::abs(duty_cycles_[1])) {
-      ROS_FATAL("Duty cycles should be between [-1, 1]");
-      exit(1);
-    }
+	// [0] corresponds to left wheel, [1] corresponds to right wheel
+	void dutyCyclesCallback(robp_interfaces::msg::DutyCycles const &msg)
+	{
+		duty_cycles_[0]  = msg.duty_cycle_left;
+		duty_cycles_[1]  = msg.duty_cycle_right;
+		duty_cyles_time_ = this->now();
+	}
 
-    // [0] corresponds to left wheel, [1] corresponds to right wheel
-    std::vector<double> wheel_angular_velocities(2, 0.0);
-    std::vector<int> abs_encoders(2, 0);
-    std::vector<int> diff_encoders(2, 0);
+	void updateMotors()
+	{
+		// if more than 0.5 seconds have passed and no messages have been received,
+		// shutdown the motors
+		if ((this->now() - duty_cyles_time_).seconds() > 0.5) {
+			duty_cycles_[0] = 0.0;
+			duty_cycles_[1] = 0.0;
+		}
 
-    std::vector<int> pwm{static_cast<int>(255 * duty_cycles_[0]),
-                         static_cast<int>(255 * duty_cycles_[1])};
+		if (1.0 < std::abs(duty_cycles_[0]) || 1.0 < std::abs(duty_cycles_[1])) {
+			RCLCPP_FATAL(this->get_logger(), "Duty cycles should be between [-1, 1]");
+			exit(1);
+		}
 
-    kobuki_motors_->update(pwm, wheel_angular_velocities, abs_encoders,
-                           diff_encoders);
+		// [0] corresponds to left wheel, [1] corresponds to right wheel
+		std::vector<double> wheel_angular_velocities(2, 0.0);
+		std::vector<int>    abs_encoders(2, 0);
+		std::vector<int>    diff_encoders(2, 0);
 
-    // publish encoders
-    encoders_msg_.header.stamp = ros::Time::now();
-    encoders_msg_.encoder_left = abs_encoders[0];
-    encoders_msg_.encoder_right = abs_encoders[1];
-    encoders_msg_.delta_encoder_left = diff_encoders[0];
-    encoders_msg_.delta_encoder_right = diff_encoders[1];
-    encoders_msg_.delta_time_left = 1000.0 * 1.0 / 10.0;
-    encoders_msg_.delta_time_right = 1000.0 * 1.0 / 10.0;
+		std::vector<int> pwm{static_cast<int>(255 * duty_cycles_[0]),
+		                     static_cast<int>(255 * duty_cycles_[1])};
 
-    encoders_pub_.publish(encoders_msg_);
+		kobuki_motors_->update(pwm, wheel_angular_velocities, abs_encoders, diff_encoders);
 
-    // calculate kinematics and send twist to robot simulation node
-    geometry_msgs::Twist twist_msg;
+		// publish encoders
+		encoders_msg_.header.stamp        = this->now();
+		encoders_msg_.encoder_left        = abs_encoders[0];
+		encoders_msg_.encoder_right       = abs_encoders[1];
+		encoders_msg_.delta_encoder_left  = diff_encoders[0];
+		encoders_msg_.delta_encoder_right = diff_encoders[1];
 
-    twist_msg.linear.x =
-        (wheel_angular_velocities[1] + wheel_angular_velocities[0]) *
-        wheel_radius_ / 2.0;
-    twist_msg.angular.z =
-        (wheel_angular_velocities[1] - wheel_angular_velocities[0]) *
-        wheel_radius_ / base_;
+		encoders_pub_->publish(encoders_msg_);
 
-    twist_pub_.publish(twist_msg);
-  }
+		// calculate kinematics and send twist to robot simulation node
+		geometry_msgs::msg::Twist twist_msg;
+
+		twist_msg.linear.x =
+		    (wheel_angular_velocities[1] + wheel_angular_velocities[0]) * wheel_radius_ / 2.0;
+		twist_msg.angular.z = (wheel_angular_velocities[1] - wheel_angular_velocities[0]) *
+		                      wheel_radius_ / base_;
+
+		twist_pub_->publish(twist_msg);
+	}
 
  private:
-  std::unique_ptr<KobukiMotors> kobuki_motors_;
+	rclcpp::TimerBase::SharedPtr timer_;
 
-  ros::Publisher encoders_pub_;
-  ros::Publisher twist_pub_;
+	std::unique_ptr<KobukiMotors> kobuki_motors_;
 
-  ros::Subscriber duty_cycles_sub_;
+	rclcpp::Publisher<robp_interfaces::msg::Encoders>::SharedPtr encoders_pub_;
+	rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr      twist_pub_;
 
-  // [0] corresponds to left wheel, [1] corresponds to right wheel
-  std::array<double, 2> duty_cycles_{};
-  ros::Time duty_cyles_time_{ros::Time::now()};
+	rclcpp::Subscription<robp_interfaces::msg::DutyCycles>::SharedPtr duty_cycles_sub_;
 
-  robp_msgs::Encoders encoders_msg_;
+	// [0] corresponds to left wheel, [1] corresponds to right wheel
+	std::array<double, 2> duty_cycles_{};
+	rclcpp::Time          duty_cyles_time_;
 
-  double wheel_radius_{0.0352};
-  double base_{0.23};
+	robp_interfaces::msg::Encoders encoders_msg_;
+
+	double wheel_radius_{0.0352};
+	double base_{0.23};
 };
 
-int main(int argc, char **argv) {
-  ros::init(argc, argv, "motors_node");
-  ros::NodeHandle nh;
-
-  Motors motors(nh);
-
-  // Control @ 10 Hz
-  ros::Rate r(10.0);
-  while (nh.ok()) {
-    motors.updateMotors();
-    r.sleep();
-    ros::spinOnce();
-  }
-
-  return 0;
+int main(int argc, char *argv[])
+{
+	rclcpp::init(argc, argv);
+	rclcpp::spin(std::make_shared<Motors>());
+	rclcpp::shutdown();
+	return 0;
 }
--- robp_boot_camp_world/src/world.cpp
+++ robp_boot_camp_world/src/world.cpp
@@ -1,124 +1,135 @@
 // ROS
 #include <angles/angles.h>
-#include <geometry_msgs/TransformStamped.h>
-#include <ros/ros.h>
-#include <std_srvs/Empty.h>
 #include <tf2/LinearMath/Quaternion.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
-#include <visualization_msgs/Marker.h>
+
+#include <geometry_msgs/msg/transform_stamped.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/empty.hpp>
+#include <std_srvs/srv/empty.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <visualization_msgs/msg/marker.hpp>
 
 // STL
 #include <cmath>
+#include <memory>
 #include <random>
 
-class World {
+class World : public rclcpp::Node
+{
  public:
-  World(ros::NodeHandle &nh) : tf_listener_(tf_buffer_) {
-    vis_pub_ = nh.advertise<visualization_msgs::Marker>("wall_marker", 0, true);
-    reset_pub_ = nh.advertise<std_msgs::Empty>("reset_distance", 0);
-    reset_srv_ = nh.advertiseService("reset_world", &World::reset, this);
-  }
+	World() : Node("world")
+	{
+		tf_buffer_   = std::make_unique<tf2_ros::Buffer>(this->get_clock());
+		tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
 
-  void publish(tf2::Transform t = tf2::Transform()) {
-    // Create random number generators
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_real_distribution<> dis(angles::from_degrees(-20.0),
-                                         angles::from_degrees(20.0));
+		vis_pub_ = this->create_publisher<visualization_msgs::msg::Marker>("wall_marker", 0);
+		reset_pub_ = this->create_publisher<std_msgs::msg::Empty>("reset_distance", 0);
+		reset_srv_ = this->create_service<std_srvs::srv::Empty>(
+		    "reset_world",
+		    std::bind(&World::reset, this, std::placeholders::_1, std::placeholders::_2));
 
-    double angle_z = dis(gen);
+		wall_marker_.header.frame_id = "odom";
+		// wall_marker_.header.stamp = this->now();
+		wall_marker_.ns      = "world";
+		wall_marker_.id      = 0;
+		wall_marker_.type    = visualization_msgs::msg::Marker::CUBE;
+		wall_marker_.action  = visualization_msgs::msg::Marker::ADD;
+		wall_marker_.scale.x = 500.0;
+		wall_marker_.scale.y = 0.01;
+		wall_marker_.scale.z = 0.2;
+		wall_marker_.color.a = 1.0;
+		wall_marker_.color.r = (255.0 / 255.0);
+		wall_marker_.color.g = (0.0 / 255.0);
+		wall_marker_.color.b = (0.0 / 255.0);
+		setRandom();
 
-    visualization_msgs::Marker wall_marker;
-    wall_marker.header.frame_id = "odom";
-    wall_marker.header.stamp = ros::Time();
-    wall_marker.ns = "world";
-    wall_marker.id = 0;
-    wall_marker.type = visualization_msgs::Marker::CUBE;
-    wall_marker.action = visualization_msgs::Marker::ADD;
-    wall_marker.scale.x = 500.0;
-    wall_marker.scale.y = 0.01;
-    wall_marker.scale.z = 0.2;
-    wall_marker.color.a = 1.0;
-    wall_marker.color.r = (255.0 / 255.0);
-    wall_marker.color.g = (0.0 / 255.0);
-    wall_marker.color.b = (0.0 / 255.0);
+		using namespace std::chrono_literals;
+		timer_ = this->create_wall_timer(100ms, std::bind(&World::publish, this));
+	}
 
-    tf2::Quaternion q = t.getRotation();
-    tf2::Vector3 p = t.inverse().getOrigin();
-
-    ROS_INFO("Robot angle: %f", q.getAngle());
-    ROS_INFO("Robot axis: %f %f %f", q.getAxis().getX(), q.getAxis().getY(),
-             q.getAxis().getZ());
-    ROS_INFO("Robot position: %f %f %f", t.getOrigin().getX(),
-             t.getOrigin().getY(), t.getOrigin().getZ());
-
-    if (angles::from_degrees(5.0) >= angle_z &&
-        angles::from_degrees(0.0) <= angle_z) {
-      angle_z += angles::from_degrees(5.0);
-    }
-
-    if (angles::from_degrees(-5.0) <= angle_z &&
-        angles::from_degrees(0.0) >= angle_z) {
-      angle_z -= angles::from_degrees(5.0);
-    }
-
-    angle_z += q.getAngle() * (-q.getAxis().getZ());
-
-    p.setX(p.getX() - 0.4 * std::sin(angle_z));
-    p.setY(p.getY() + 0.4 * std::cos(angle_z));
-    p.setZ(std::isnan(p.getZ()) ? 0.1 : p.getZ() + 0.1);
-    q.setRotation(tf2::Vector3(0, 0, 1), angle_z);
-
-    tf2::toMsg(p, wall_marker.pose.position);
-    wall_marker.pose.orientation = tf2::toMsg(q);
-
-    vis_pub_.publish(wall_marker);
-  }
+	void publish() { vis_pub_->publish(wall_marker_); }
 
  private:
-  bool reset(std_srvs::Empty::Request &req, std_srvs::Empty::Response &res) {
-    reset_pub_.publish(std_msgs::Empty());
+	double randomAngle() const
+	{
+		// Create random number generators
+		static std::random_device               rd;
+		static std::mt19937                     gen(rd());
+		static std::uniform_real_distribution<> dis(angles::from_degrees(-20.0),
+		                                            angles::from_degrees(20.0));
+		return dis(gen);
+	}
 
-    tf2::Transform transform;
-    bool got_transform = false;
-    while (!got_transform) {
-      try {
-        tf2::fromMsg(
-            tf_buffer_.lookupTransform("base_link", "odom", ros::Time(0))
-                .transform,
-            transform);
-        got_transform = true;
-      } catch (tf2::TransformException &ex) {
-        ROS_WARN("%s", ex.what());
-        ros::Duration(1.0).sleep();
-      }
-    }
+	void setRandom(tf2::Transform t = tf2::Transform())
+	{
+		auto angle_z = randomAngle();
 
-    publish(transform);
+		tf2::Quaternion q = t.getRotation();
+		tf2::Vector3    p = t.inverse().getOrigin();
 
-    return true;
-  }
+		RCLCPP_INFO(this->get_logger(), "Robot angle: %f", q.getAngle());
+		RCLCPP_INFO(this->get_logger(), "Robot axis: %f %f %f", q.getAxis().getX(),
+		            q.getAxis().getY(), q.getAxis().getZ());
+		RCLCPP_INFO(this->get_logger(), "Robot position: %f %f %f", t.getOrigin().getX(),
+		            t.getOrigin().getY(), t.getOrigin().getZ());
+
+		if (angles::from_degrees(5.0) >= angle_z && angles::from_degrees(0.0) <= angle_z) {
+			angle_z += angles::from_degrees(5.0);
+		}
+
+		if (angles::from_degrees(-5.0) <= angle_z && angles::from_degrees(0.0) >= angle_z) {
+			angle_z -= angles::from_degrees(5.0);
+		}
+
+		angle_z += q.getAngle() * (-q.getAxis().getZ());
+
+		p.setX(p.getX() - 0.4 * std::sin(angle_z));
+		p.setY(p.getY() + 0.4 * std::cos(angle_z));
+		p.setZ(std::isnan(p.getZ()) ? 0.1 : p.getZ() + 0.1);
+		q.setRotation(tf2::Vector3(0, 0, 1), angle_z);
+
+		tf2::toMsg(p, wall_marker_.pose.position);
+		wall_marker_.pose.orientation = tf2::toMsg(q);
+	}
+
+	void reset(std::shared_ptr<std_srvs::srv::Empty::Request> const /* request */,
+	           std::shared_ptr<std_srvs::srv::Empty::Response> /* response */)
+	{
+		reset_pub_->publish(std_msgs::msg::Empty());
+
+		tf2::Transform transform{};
+		try {
+			tf2::fromMsg(
+			    tf_buffer_->lookupTransform("base_link", "odom", rclcpp::Time(0)).transform,
+			    transform);
+
+		} catch (tf2::TransformException &ex) {
+			RCLCPP_WARN(this->get_logger(), "%s", ex.what());
+		}
+
+		setRandom(transform);
+	}
 
  private:
-  tf2_ros::Buffer tf_buffer_;
-  tf2_ros::TransformListener tf_listener_;
+	rclcpp::TimerBase::SharedPtr timer_;
 
-  ros::Publisher vis_pub_;
-  ros::Publisher reset_pub_;
+	visualization_msgs::msg::Marker wall_marker_;
 
-  ros::ServiceServer reset_srv_;
+	std::shared_ptr<tf2_ros::TransformListener> tf_listener_{nullptr};
+	std::unique_ptr<tf2_ros::Buffer>            tf_buffer_;
+
+	rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr vis_pub_;
+	rclcpp::Publisher<std_msgs::msg::Empty>::SharedPtr            reset_pub_;
+
+	rclcpp::Service<std_srvs::srv::Empty>::SharedPtr reset_srv_;
 };
 
-int main(int argc, char **argv) {
-  // Set up ROS
-  ros::init(argc, argv, "world_node");
-  ros::NodeHandle nh;
-
-  World world(nh);
-  world.publish();
-
-  ros::spin();
-
-  return 0;
+int main(int argc, char *argv[])
+{
+	rclcpp::init(argc, argv);
+	rclcpp::spin(std::make_shared<World>());
+	rclcpp::shutdown();
+	return 0;
 }
